31997608df45490966b7cc2098a2341aa14304c0
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-08-10 16:20:06 +0300

    add screen and engine pipeline

---
+++ engine/src/engine.cpp
@@ -0,0 +1,36 @@
+//
+// Created by ozgur on 8/8/2024.
+//
+
+#include "../include/engine.h"
+#include "../include/screen.h"
+#include "../include/graphic_pipeline.h"
+
+namespace zgr
+{
+
+    engine::engine()
+    {
+        init();
+    }
+
+    engine::~engine()
+    {
+        delete engine_screen;
+        delete engine_pipeline;
+    }
+
+    void engine::run()
+    {
+        while (!engine_screen->should_close())
+        {
+            glfwPollEvents();
+        }
+    }
+
+    void engine::init()
+    {
+        engine_screen = new screen();
+        engine_pipeline = new graphic_pipeline("shaders/vertex_shader.vert.spv", "shaders/fragment_shader.frag.spv");
+    }
+}
