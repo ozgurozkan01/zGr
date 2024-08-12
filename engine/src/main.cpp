b4c20c21d11cc4b6be660618183ec6233ae3500f
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-08-12 10:52:35 +0300

    implement engine run with try-catch to see bug

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,10 +1,20 @@
 #include "../../engine/include/engine.h"
+#include <exception>
+#include <iostream>
 
 int main()
 {
     zgr::engine engine;
 
-    engine.run();
+    try
+    {
+        engine.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        EXIT_FAILURE;
+    }
 
-    return 0;
+    EXIT_SUCCESS;
 }
