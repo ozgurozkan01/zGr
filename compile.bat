85106028cf3e4346c137559e782db5804d57a3e6
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-09-15 18:11:07 +0300

    remove redundant batch file

--- compile.bat
+++
@@ -1,3 +0,0 @@
-C:\VulkanSDK\1.3.290.0\Bin\glslc.exe shaders\vertex_shader.vert -o shaders\vertex_shader.vert.spv
-C:\VulkanSDK\1.3.290.0\Bin\glslc.exe shaders\fragment_shader.frag -o shaders\fragment_shader.frag.spv
-pause
